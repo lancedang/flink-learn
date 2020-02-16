f2c0d3a6f09559c3b9feac1dc2cc14f35ad5ca96
qiankai <qiankai07@meituan.com>
2020-02-16 18:07:44 +0800

    初始化 flink helloworld demo

---
+++ readme.md
@@ -0,0 +1,2 @@
+# 说明
+flink 入门HelloWorld
---
+++ src/main/java/com/dangdang/flink/StreamingJob.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dangdang.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * <p>For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class StreamingJob {
+
+	public static void main(String[] args) throws Exception {
+		// set up the streaming execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		String host = "localhost";
+		int port = 9000;
+
+		DataStreamSource<String> stringDataStreamSource = env.socketTextStream(host, port);
+
+		SingleOutputStreamOperator<Tuple2<String, Integer>> sum = stringDataStreamSource.flatMap(new LineSplitter())
+				.keyBy(0)
+				.sum(1);
+
+		sum.print();
+
+		// execute program
+		env.execute("word count");
+	}
+
+	public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+		@Override
+		public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) {
+			String[] tokens = s.toLowerCase().split("\\W+");
+
+			for (String token: tokens) {
+				if (token.length() > 0) {
+					collector.collect(new Tuple2<String, Integer>(token, 1));
+				}
+			}
+		}
+	}
+}
+
+
